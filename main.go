4f5009b071a3e7e391bf9c8408444d0464d3c9f0
xiaodian <1004218964@qq.com>
2023-07-06 16:40:30 +0800

    Add Http GetUI&Function

--- main.go
+++ main.go
@@ -4,22 +4,52 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	rsps, err := http.Get("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=18618224510")
+	a := app.New()
+	win := a.NewWindow("HttpAPITest")
+	win.Resize(fyne.NewSize(1000, 50))
+	win.SetContent(APIUI())
+	win.ShowAndRun()
+}
+
+func APIUI() *fyne.Container {
+	editURLBox := widget.NewEntry()
+	editURLBox.SetPlaceHolder("Please Enter URL")
+	APISelect := widget.NewSelect([]string{"GET", "POST"}, ChooseAPISelect)
+	SendButton := widget.NewButton("Send", func() { SendButton(editURLBox.Text) })
+	SendButton.Importance = widget.HighImportance
+	HBox := container.NewGridWithColumns(3, APISelect, editURLBox, SendButton)
+	return HBox
+}
+
+func ChooseAPISelect(API string) {
+	fmt.Println(API)
+}
+
+func SendButton(URL string) {
+	body := httpGet(URL)
+	fmt.Println(string(body))
+}
+
+func httpGet(url string) []byte {
+	rsps, err := http.Get(url)
 	if err != nil {
 		fmt.Println("http get error", err)
-		return
+		return nil
 	}
 	defer rsps.Body.Close()
 
 	body, err := ioutil.ReadAll(rsps.Body)
-
 	if err != nil {
 		fmt.Println("read error", err)
-		return
+		return nil
 	}
-
-	fmt.Println(string(body))
+	return body
 }

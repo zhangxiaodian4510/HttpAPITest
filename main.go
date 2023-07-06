6f0810c816de81e92d961a28f19c524a890b2493
xiaodian <1004218964@qq.com>
2023-07-06 13:13:14 +0800

    add http function

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func main() {
+	rsps, err := http.Get("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=18618224510")
+	if err != nil {
+		fmt.Println("http get error", err)
+		return
+	}
+	defer rsps.Body.Close()
+
+	body, err := ioutil.ReadAll(rsps.Body)
+
+	if err != nil {
+		fmt.Println("read error", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }

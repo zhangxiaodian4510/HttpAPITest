6b0d56abd7555804e0431ca1e2a2955e5303b413
zhangxiaodian <z1004218964@gmail.com>
2023-07-07 00:14:49 +0800

    Fix Chinese Error

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module http_api_test
 
 go 1.20
 
-require fyne.io/fyne/v2 v2.3.5
+require (
+	fyne.io/fyne/v2 v2.3.5
+	github.com/flopp/go-findfont v0.1.0
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20230602210930-b6a2d6ca2a7b // indirect
--- main.go
+++ main.go
@@ -4,16 +4,28 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/flopp/go-findfont"
 )
 
+var response *widget.Label
+
 func main() {
+	fontpaths := findfont.List()
+	for _, path := range fontpaths {
+		if strings.Contains(path, "STSONG.TTF") {
+			os.Setenv("FYNE_FONT", path)
+		}
+	}
 	a := app.New()
 	win := a.NewWindow("HttpAPITest")
+	response = widget.NewLabel("Response")
 	win.Resize(fyne.NewSize(1000, 50))
 	win.SetContent(APIUI())
 	win.ShowAndRun()
@@ -23,9 +35,10 @@ func APIUI() *fyne.Container {
 	editURLBox := widget.NewEntry()
 	editURLBox.SetPlaceHolder("Please Enter URL")
 	APISelect := widget.NewSelect([]string{"GET", "POST"}, ChooseAPISelect)
+	APISelect.SetSelected("GET")
 	SendButton := widget.NewButton("Send", func() { SendButton(editURLBox.Text) })
 	SendButton.Importance = widget.HighImportance
-	HBox := container.NewGridWithColumns(3, APISelect, editURLBox, SendButton)
+	HBox := container.NewGridWithColumns(3, APISelect, editURLBox, SendButton, response)
 	return HBox
 }
 
@@ -35,19 +48,25 @@ func ChooseAPISelect(API string) {
 
 func SendButton(URL string) {
 	body := httpGet(URL)
+	if body == nil {
+		return
+	}
+	response.SetText(string(body))
 	fmt.Println(string(body))
 }
 
 func httpGet(url string) []byte {
 	rsps, err := http.Get(url)
 	if err != nil {
-		fmt.Println("http get error", err)
+		response.SetText(err.Error())
+		fmt.Println(err.Error())
 		return nil
 	}
 	defer rsps.Body.Close()
 
 	body, err := ioutil.ReadAll(rsps.Body)
 	if err != nil {
+		response.SetText(err.Error())
 		fmt.Println("read error", err)
 		return nil
 	}
